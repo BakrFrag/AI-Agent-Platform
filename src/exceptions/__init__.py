d71a1c4b30152a488c403d2282814a33cdfe7161
bk <bakr.frag@gmail.com>
2025-11-30 07:05:45 +0200

    remove iunused code

--- src/exceptions/__init__.py
+++ src/exceptions/__init__.py
@@ -1,8 +1,5 @@
 from src.exceptions.handler import register_global_exception_handlers
 
 __all__ = [
-    "register_sqlalchemy_handler",
-    "register_openai_handler",
-    "register_http_handler",
     "register_global_exception_handlers"
 ]
